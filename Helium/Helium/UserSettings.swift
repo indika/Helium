e1e062a9824157fc1ccbcbe64b29a68569eb0520
Viktor Oreshkin <stek29@users.noreply.github.com>
2017-05-09 20:51:49 +0300

    Minor changes in UserSettings
    Signed-off-by: Viktor Oreshkin <stek29@users.noreply.github.com>

--- Helium/Helium/UserSettings.swift
+++ Helium/Helium/UserSettings.swift
@@ -8,40 +8,40 @@
 
 import Foundation
 
-internal class Setting<T> {
-	let key: String
-	let defaultValue: T
-
-	init(_ userDefaultsKey: String, defaultValue: T) {
-		self.key = userDefaultsKey
-		self.defaultValue = defaultValue
-	}
-
-	var value: T {
-		get {
-			return self.get()
-		}
-		set (value) {
-			self.set(value)
-		}
-	}
-
-	private func get() -> T {
-		if let value = UserDefaults.standard.object(forKey: self.key) as? T {
-			return value
-		} else {
-			// Sets default value if failed
-			set(self.defaultValue)
-			return self.defaultValue
-		}
-	}
-
-	private func set(_ value: T) {
-		UserDefaults.standard.set(value as Any, forKey: self.key)
-	}
-}
-
 internal struct UserSettings {
+	internal class Setting<T> {
+		private let key: String
+		private let defaultValue: T
+
+		init(_ userDefaultsKey: String, defaultValue: T) {
+			self.key = userDefaultsKey
+			self.defaultValue = defaultValue
+		}
+
+		var value: T {
+			get {
+				return self.get()
+			}
+			set (value) {
+				self.set(value)
+			}
+		}
+
+		private func get() -> T {
+			if let value = UserDefaults.standard.object(forKey: self.key) as? T {
+				return value
+			} else {
+				// Sets default value if failed
+				set(self.defaultValue)
+				return self.defaultValue
+			}
+		}
+
+		private func set(_ value: T) {
+			UserDefaults.standard.set(value as Any, forKey: self.key)
+		}
+	}
+
 	static let disabledMagicURLs = Setting<Bool>("disabledMagicURLs", defaultValue: false)
 
 	static let disabledFullScreenFloat = Setting<Bool>("disabledFullScreenFloat", defaultValue: false)
